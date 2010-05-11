e6639e23203663204add1286d6172edf731739f6
huangcongyu2006 <huangcongyu2006@8d33c5fa-191d-556e-b74c-84ded499624a>
2010-05-11 09:29:53 +0000

    nomsg

--- src/configure.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SWIFTD_CONFIGURE_H
-#define __SWIFTD_CONFIGURE_H
-
-#include "base.h"
-
-int config_setdefaults(server *srv);
-
-#endif
--- src/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MAIN_H
-#define __MAIN_H
-
-#include "base.h"
-
-
-#endif
